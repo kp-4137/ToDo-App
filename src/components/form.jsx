f41cc4d9eed3b395adb1b5bff1f029eb86473e1f
Kishan Patel <kishan@Kishans-MacBook-Air.local>
2022-04-22 04:36:39 -0700

    Updated form component to have multiple input fields

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -1,8 +1,21 @@
 const Form = () => {
     return(
         <form>
-            <input type="text" className="todo-input" />
-            <button className="todo-button" type="submit">Add</button>
+            <div className="form-control">
+                <label htmlFor="title">Title</label>
+                <input type="text" className="todo-input" name="title"/>
+            </div>
+            <div className="form-control">
+                <label htmlFor="description">Description</label>
+                <textarea className="todo-input" name="description"/>
+            </div>
+            <div className="form-control">
+                <label htmlFor="deadline">Deadline</label>
+                <input type="date" className="todo-input" name="deadline"/>
+            </div>
+            <button className="todo-button" type="submit" style={{color:"white", backgroundColor:"green", border:"none"}}>
+                Add
+            </button>
             <div className="select">
                 <select name="todos" className="filter-todo">
                     <option value="all">All</option>
