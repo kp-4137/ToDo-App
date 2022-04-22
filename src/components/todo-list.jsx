78e9a3571b5f56854278d4a428e3389214fe41c2
Kishan Patel <kishan@Kishans-MacBook-Air.local>
2022-04-22 11:07:39 -0700

    Added list of todos as state of todo-list

--- src/components/todo-list.jsx
+++ src/components/todo-list.jsx
@@ -1,13 +1,35 @@
+import { useState } from "react";
 import Todo from "./todo";
 
 const TodoList = () => {
+    const [todos, setTodos] = useState(
+        [
+            {
+                id: 1,
+                title: "Learn React",
+                description: "A JavaScript Library for building User Interfaces.",
+                deadline: "04/30/2022"
+            },
+            {
+                id: 2,
+                title: "Create Project",
+                description: "A todo app using React",
+                deadline: "04/23/2022"
+            },
+            {
+                id: 3,
+                title: "Learn Swift",
+                description: "A programming language to create native applications for Apple devices.",
+                deadline: "05/31/2022"
+            }
+        ]
+    );
+
     return(
         <div className="todo-container">
             <ul className="todo-list">
-                <Todo />
-                <Todo />
-                <Todo title="Learn React" />
-                <Todo title="Create Project" description="A todo app using React" deadline="04/23/2022" />
+                {todos.map((todo) => (<Todo key={todo.id} title={todo.title} description={todo.description} deadline={todo.deadline}/>)
+                )}
             </ul>
         </div>
     );
