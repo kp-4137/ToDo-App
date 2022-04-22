543d7661d8853f9729fe91177e009278d5c281ab
Kishan Patel <kishan@Kishans-MacBook-Air.local>
2022-04-22 03:18:57 -0700

    Added form and todo-list components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import './App.css';
+import "./App.css";
+import Form from "./components/form";
+import TodoList from "./components/todo-list";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        ToDo App
+        <h1>Kishan's ToDo App</h1>
       </header>
+      <Form />
+      <TodoList />
     </div>
   );
 }
---
+++ src/components/form.jsx
@@ -0,0 +1,17 @@
+const Form = () => {
+    return(
+        <form>
+            <input type="text" className="todo-input" />
+            <button className="todo-button" type="submit">Add</button>
+            <div className="select">
+                <select name="todos" className="filter-todo">
+                    <option value="all">All</option>
+                    <option value="completed">Completed</option>
+                    <option value="incomplete">Incomplete</option>
+                </select>
+            </div>
+        </form>
+    );
+}
+
+export default Form;
