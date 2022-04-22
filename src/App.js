d8356c93f83c9d4352c3211295385fc4464ca835
Kishan Patel <kishan@Kishans-MacBook-Air.local>
2022-04-22 02:18:53 -0700

    ToDo React App Started

--- src/App.js
+++ src/App.js
@@ -3,13 +3,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
       <header className="App-header">
         ToDo App
       </header>
-=======
-      ToDo App
->>>>>>> 6c19ea4 (ToDo React App Started)
     </div>
   );
 }
