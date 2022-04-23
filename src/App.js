66a92837c4df538f90699fed6963879f81baf023
Kishan Patel <kishan@Kishans-MacBook-Air.local>
2022-04-22 18:37:06 -0700

    Moved state from todo-list component to App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,42 @@
+import { useState } from "react";
 import "./App.css";
 import Header  from "./components/header";
 import Form from "./components/form";
 import TodoList from "./components/todo-list";
 
 function App() {
+  const [todos, setTodos] = useState(
+    [
+        {
+            id: 1,
+            title: "Learn React",
+            description: "A JavaScript Library for building User Interfaces.",
+            deadline: "04/30/2022"
+        },
+        {
+            id: 2,
+            title: "Create Project",
+            description: "A todo app using React",
+            deadline: "04/23/2022"
+        },
+        {
+            id: 3,
+            title: "Learn Swift",
+            description: "A programming language to create native applications for Apple devices.",
+            deadline: "05/31/2022"
+        }
+    ]
+  );
+
+  const deleteTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  }
+
   return (
     <div className="App">
       <Header />
       <Form />
-      <TodoList />
+      <TodoList todos={todos} onDeleteTodo={deleteTodo}/>
     </div>
   );
 }
--- src/components/todo-list.jsx
+++ src/components/todo-list.jsx
@@ -1,38 +1,10 @@
-import { useState } from "react";
 import Todo from "./todo";
 
-const TodoList = () => {
-    const [todos, setTodos] = useState(
-        [
-            {
-                id: 1,
-                title: "Learn React",
-                description: "A JavaScript Library for building User Interfaces.",
-                deadline: "04/30/2022"
-            },
-            {
-                id: 2,
-                title: "Create Project",
-                description: "A todo app using React",
-                deadline: "04/23/2022"
-            },
-            {
-                id: 3,
-                title: "Learn Swift",
-                description: "A programming language to create native applications for Apple devices.",
-                deadline: "05/31/2022"
-            }
-        ]
-    );
-
-    const deleteTodo = (id) => {
-        setTodos(todos.filter((todo) => todo.id !== id));
-    }
-
+const TodoList = ({todos, onDeleteTodo}) => {
     return(
         <div className="todo-container">
             <ul className="todo-list">
-                {todos.map((todo) => (<Todo key={todo.id} todo={todo} onDelete={deleteTodo}/>)
+                {todos.map((todo) => (<Todo key={todo.id} todo={todo} onDelete={onDeleteTodo}/>)
                 )}
             </ul>
         </div>
