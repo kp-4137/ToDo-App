7cd8895de2526a737e0af6a6bde7e22f674a1525
Kishan Patel <kishan@Kishans-MacBook-Air.local>
2022-04-24 21:40:42 -0700

    Moved the initial state to a separate JSON file.

--- src/contexts/todoContext.jsx
+++ src/contexts/todoContext.jsx
@@ -1,27 +1,9 @@
 import { useReducer, createContext } from "react";
 import AppReducer from "./appReducer";
+import initialTodos from "./initialTodos.json";
 
 const initialState = {
-    todos: [
-        {
-            id: 1,
-            title: "Learn React",
-            description: "A JavaScript Library for building User Interfaces.",
-            deadline: "04/30/2022"
-        },
-        {
-            id: 2,
-            title: "Create Project",
-            description: "A todo app using React",
-            deadline: "04/23/2022"
-        },
-        {
-            id: 3,
-            title: "Learn Swift",
-            description: "A programming language to create native applications for Apple devices.",
-            deadline: "05/31/2022"
-        }
-    ]
+    todos: initialTodos
 }
 
 export const TodoContext = createContext(initialState);
